7f8cfb3c8b1eb1b719b1fed26fe4a74857272852
Sergey-Rebrov <sergey.rebrov@gmail.com>
2017-02-11 18:49:11 +0700

    JavaRushTasks

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -11,28 +11,49 @@ public class Solution {
         long[] result = null;
         List<Long> longList = new ArrayList<>();
 
-        int[][] massiv = new int[10][String.valueOf(N).length()];
+        long[][] massiv = new long[10][String.valueOf(N).length()];
 
         for (int i = 0; i < massiv.length; i++)
-            for (int j = 0; j < massiv[0].length; j++)
-                massiv[i][j] = (int)Math.pow(i, j + 1);
-
-        long number = 0L;
-
-        while (number <= N)
-        {
-            char[] buffer = String.valueOf(number).toCharArray();
-            long sum = 0;
-            for (char symbol : buffer)
-            {
-                sum += massiv[Integer.parseInt(String.valueOf(symbol))][buffer.length - 1];
+            for (int j = 0; j < massiv[0].length; j++) {
+                long tmp = i;
+                for (int k = 0; k < j; k++)
+                    tmp = tmp * i;
+                massiv[i][j] = tmp;
             }
+
+        long number = 1L;
+
+        while (number < N) {
+
+            long sum = 0;
+
+            long num = number;
+            int degree = 0;
+            while (num != 0) {
+                num = num / 10;
+                degree++;
+            }
+
+            boolean flag = true;
+            int tmp1 = (int) (number / 10);
+            int tmp2 = (int) (number % 10);
+
+            while (flag) {
+                if (tmp1 == 0)
+                    flag = false;
+
+                if (tmp2 != 0)
+                    sum += massiv[tmp2][degree - 1];
+
+                tmp2 = tmp1 % 10;
+                tmp1 = tmp1 / 10;
+            }
+
 
             if (sum == number) {
                 longList.add(number);
                 System.out.println(number);
             }
-
 
             number++;
         }
@@ -44,10 +65,12 @@ public class Solution {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
-        getNumbers(40000000);
+        getNumbers(Integer.MAX_VALUE);
         long end = System.currentTimeMillis();
         System.out.println(end - start + " millisecond");
+        System.out.println("memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024) + " mb");
+
     }
 }
