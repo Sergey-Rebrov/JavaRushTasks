edc551d0e0ece33d577aacd2e2e7a92fbb43dd04
Sergey-Rebrov <sergey.rebrov@gmail.com>
2017-02-13 21:26:42 +0700

    JavaRushTasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task30.task3010;
+
+/* 
+Минимальное допустимое основание системы счисления
+*/
+
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        String string = args[0];
+        if (string.matches("^[a-zA-Z0-9]+$")) {
+            for (int i = 2; i < 37; i++) {
+                try {
+                    BigInteger bigInteger = new BigInteger(string, i);
+                    System.out.println(String.valueOf(i));
+                    break;
+                } catch (Exception ignore) {
+                }
+            }
+        } else
+            System.out.println("incorrect");
+    }
+}
